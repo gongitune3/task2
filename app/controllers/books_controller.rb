00462377efd6005cdf3040a586d4041a36beb846
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-05-12 21:32:12 +0900

    add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,45 @@
 class BooksController < ApplicationController
+  def top
+
+  end
+  
   def show
+    @book = Book.find(params[:id])
   end
 
-  def new
+  def index
     @books = Book.all
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+      @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to new_blog_path
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to new_book_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
